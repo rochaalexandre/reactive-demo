766dcdb836649cb0efbba0d35ca7cac170fafebb
Alexandre Rocha <alexandre.rocha@clevertech.biz>
2024-08-09 17:15:36 -0300

    feat: add stream delay

--- src/main/java/com/example/reactivedemo/controller/PostController.java
+++ src/main/java/com/example/reactivedemo/controller/PostController.java
@@ -1,15 +1,14 @@
 package com.example.reactivedemo.controller;
 
 import com.example.reactivedemo.model.Post;
-import com.example.reactivedemo.model.User;
 import com.example.reactivedemo.service.PostService;
-import com.example.reactivedemo.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Map;
 
 @AllArgsConstructor
@@ -26,7 +25,7 @@ public class PostController {
 
     @GetMapping(path = "published", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Post> getAllPublishedPosts() {
-        return this.postService.findAllPublished().concatWith(this.postService.getRecentPublishedPosts());
+        return this.postService.findAllPublished().concatWith(this.postService.getRecentPublishedPosts()).delaySequence(Duration.ofSeconds(3));
     }
 
     @PostMapping
